d4d322ef39ad505fd3d0ed35c51e4803e1adce70
JoJoCrit <yataga@ya.ru>
2022-03-02 15:33:31 +0300

    задание с 4 листа

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -1,7 +1,7 @@
 package hexlet.code;
 
 public class App {
-  public static void main(String[] args) {
-    Cli.User();
-  }
+    public static void main(String[] args) {
+        Cli.aUser();
+    }
 }
--- src/main/java/hexlet/code/Cli.java
+++ src/main/java/hexlet/code/Cli.java
@@ -3,10 +3,13 @@ package hexlet.code;
 import java.util.Scanner;
 
 public class Cli {
-    public static void User() {
+    public static void aUser() {
         Scanner sc = new Scanner(System.in);
+
         System.out.println("Welcome to the Brain Games!");
+
         System.out.print("May I have your name? ");
+
         System.out.println("Hello, " + sc.next() + "!");
     }
 }
